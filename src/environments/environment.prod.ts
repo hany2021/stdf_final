7d6914aabd9997745ba7b0bd5638f5978c81f6f8
hany.nady@softprograms-eg.com <hany.nady@softprograms-eg.com>
2024-04-09 00:24:18 +0200

    stdf_final

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'stdf',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44314/',
+    redirectUri: baseUrl,
+    clientId: 'stdf_App',
+    responseType: 'code',
+    scope: 'offline_access stdf',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44314',
+      rootNamespace: 'stdf',
+    },
+  },
+} as Environment;
